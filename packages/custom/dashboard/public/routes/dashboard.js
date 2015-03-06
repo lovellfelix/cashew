c403963ce21b68e109f93c08ff14e3e71dbe33ed
Lovell Felix <lovell.felix@gmail.com>
2015-03-05 20:24:36 -0600

    Added Auth to default page

--- packages/custom/dashboard/public/routes/dashboard.js
+++ packages/custom/dashboard/public/routes/dashboard.js
@@ -2,9 +2,34 @@
 
 angular.module('mean.dashboard').config(['$stateProvider',
   function($stateProvider) {
+
+    // Check if the user is connected
+    var checkLoggedin = function($q, $timeout, $http, $location) {
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user) {
+        // Authenticated
+        if (user !== '0') $timeout(deferred.resolve);
+
+        // Not Authenticated
+        else {
+          $timeout(deferred.reject);
+          $location.url('/auth/login');
+        }
+      });
+
+      return deferred.promise;
+    };
+
+
     $stateProvider.state('dashboard example page', {
       url: '/dashboard/example',
-      templateUrl: 'dashboard/views/index.html'
+      templateUrl: 'dashboard/views/index.html',
+      resolve: {
+        loggedin: checkLoggedin
+      }
     });
   }
 ]);
