8fea42173b764bf27e221d56380f7e3ff6d873aa
Lovell Felix <lovell.felix@gmail.com>
2015-03-20 01:45:39 -0500

    Force login on entry

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -2,10 +2,12 @@
 
 var app = angular.module('core');
 
-app.controller('HomeController', ['$scope', 'Authentication',
-	function($scope,  Authentication) {
+app.controller('HomeController', ['$scope', 'Authentication', '$location',
+	function($scope,  Authentication, $location) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		if (!$scope.authentication.user) $location.path('/authentication/signin');
 
 
 	}
