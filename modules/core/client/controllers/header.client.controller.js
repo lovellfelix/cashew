5e989aec2278f0d7d6b219b56ce391d80eb81c87
Lovell Felix <lovell.felix@gmail.com>
2015-03-20 00:56:22 -0500

    Removed console log

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -36,7 +36,6 @@ app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
     $mdSidenav('left').close()
       .then(function() {
-        $log.debug('close LEFT is done');
       });
   };
 });
@@ -45,7 +44,6 @@ app.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
     $mdSidenav('right').close()
       .then(function() {
-        $log.debug('close RIGHT is done');
       });
   };
 });
