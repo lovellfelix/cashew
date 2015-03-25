9a14a5ad7003e4b5d701de726a10beb4512e095e
Lovell Felix <lovell.felix@gmail.com>
2015-03-25 00:25:01 -0500

    Added dialog with product information

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -2,48 +2,55 @@
 
 var app = angular.module('core');
 
-app.controller('HeaderController', ['$scope', '$state', '$mdSidenav', 'Authentication', 'Menus',
-	function($scope, $state, $mdSidenav, Authentication, Menus) {
-		// Expose view variables
-		$scope.$state = $state;
-		$scope.authentication = Authentication;
+app.controller('HeaderController', ['$scope', '$state', '$mdSidenav', 'Authentication', 'Menus', 'LxNotificationService',
+  function($scope, $state, $mdSidenav, Authentication, Menus, LxNotificationService) {
+    // Expose view variables
+    $scope.$state = $state;
+    $scope.authentication = Authentication;
 
-		// Get the topbar menu
-		$scope.menu = Menus.getMenu('topbar');
+    // Get the topbar menu
+    $scope.menu = Menus.getMenu('topbar');
 
-		// Toggle the menu items
-		$scope.isCollapsed = false;
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+    // Toggle the menu items
+    $scope.isCollapsed = false;
+    $scope.toggleCollapsibleMenu = function() {
+      $scope.isCollapsed = !$scope.isCollapsed;
+    };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
+    // Collapsing the menu after navigation
+    $scope.$on('$stateChangeSuccess', function() {
+      $scope.isCollapsed = false;
+    });
 
-		$scope.toggleSidenav = function(menuId) {
-	      $mdSidenav(menuId).toggle();
-	    };
+    $scope.toggleSidenav = function(menuId) {
+      $mdSidenav(menuId).toggle();
+    };
 
-	    $scope.toggleRight = function() {
-	      $mdSidenav('right').toggle();
-	    };
-	}
+    $scope.toggleRight = function() {
+      $mdSidenav('right').toggle();
+    };
+
+
+    $scope.alert = function() {
+      LxNotificationService.alert('Arawak', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet urna quis nisi sodales semper pharetra eu augue.', 'Ok', function(answer) {
+        // console.log(answer);
+      });
+    };
+
+
+  }
 ]);
 
 app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
     $mdSidenav('left').close()
-      .then(function() {
-      });
+      .then(function() {});
   };
 });
 
 app.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
     $mdSidenav('right').close()
-      .then(function() {
-      });
+      .then(function() {});
   };
 });
