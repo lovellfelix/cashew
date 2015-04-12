0209c67e6315abc6169832647c0fe47a0dd28b2e
Lovell Felix <lovell.felix@gmail.com>
2015-04-11 19:40:24 -0500

    Added settings as a module

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -5,11 +5,6 @@ angular.module('users').config(['$stateProvider',
 	function ($stateProvider) {
 		// Users state routing
 		$stateProvider.
-			state('settings', {
-				abstract: true,
-				url: '/settings',
-				templateUrl: 'modules/users/views/settings/settings.client.view.html'
-			}).
 			state('settings.profile', {
 				url: '/profile',
 				templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
