9f05faa5cd7a04a124ca563b180f88dab753c55c
Lovell Felix <lovell.felix@gmail.com>
2015-07-01 18:12:53 -0500

    Added production resources

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,17 +4,26 @@ module.exports = {
 	client: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/angular-material/angular-material.min.css',
+				'public/lib/mdi/css/materialdesignicons.min.css',
+==      'public/lib/angular-xeditable/dist/css/xeditable.css',
+				'public/lib/lumx/dist/lumx.css'
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
+				'public/lib/jquery/dist/jquery.js',
+				'public/lib/velocity/velocity.min.js',
+				'public/lib/angular/angular.js',
+				'public/lib/angular-aria/angular-aria.min.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-material/angular-material.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/angular-file-upload/angular-file-upload.min.js'
+				'public/lib/angular-file-upload/angular-file-upload.min.js',
+				'public/lib/moment/min/moment-with-locales.min.js',
+				'public/lib/lumx/dist/lumx.min.js',
+				'public/lib/angular-xeditable/dist/js/xeditable.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
